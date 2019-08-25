e9df332f80ede7ecfde9f294b1e94db899451b44
dkmillington <dale.millington@tutanota.com>
2019-08-25 22:15:32 +0100

    Player score now displays on screen.

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameStatus : MonoBehaviour
 {
@@ -8,14 +9,21 @@ public class GameStatus : MonoBehaviour
     // Configuration parameters
     [Range(0.1f, 10f)]  [SerializeField] float gameSpeed = 1f;
     [SerializeField] int pointsPerBlockDestroyed = 83;
+    [SerializeField] TextMeshProUGUI scoreText;
 
     // State variables
     [SerializeField] int currentScore = 0;
+
+    private void Start()
+    {
+        scoreText.text = currentScore.ToString();
+    }
 
     // Update is called once per frame
     void Update()
     {
         Time.timeScale = gameSpeed;
+        scoreText.text = currentScore.ToString();
     }
 
     public void AddToScore()
